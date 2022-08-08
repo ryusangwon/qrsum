7b3fbd057d0777514469c5657aab1d4f06881085
ryusangwon <r8uuuuuuuu@gmail.com>
2022-08-08 16:53:59 +0900

    erase previous codes

--- rfairseq.py
+++
@@ -1,5 +0,0 @@
-import torch
-from fairseq.models.bart import BARTModel
-
-bart = BARTModel.from_pretrained('/path/to/bart.large', checkpoint_file='model.pt')
-bart = torch.hub.load("pytorch/fairseq", args.model_file)
